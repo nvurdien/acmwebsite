a25e1d0efa37d1f1ba10b74b4c60552879fdb429
Oscar Olazabal <oscarolazabal@gmail.com>
2018-05-27 14:35:02 -0700

    add fall 2018 board

---
+++ acmwebsite/src/lists/Fall2018List.js
@@ -0,0 +1,66 @@
+import StevenImage from '../media/charles.jpg';
+import OscarImage from '../media/20170914_132704.jpg';
+import TommyImage from '../media/15844524_1297254926998904_763713103847688498_o.jpg';
+import ChantalleImage from '../media/20170825_172230.jpg';
+import JulianImage from '../media/20170825_172118.jpg';
+import DipenImage from '../media/dipen.jpg';
+import WilliamImage from '../media/william.jpg';
+
+export default [{
+    id: 1,
+    image: OscarImage,
+    name: "Oscar Olazabal",
+    title: "President",
+    description: "Management of established programs, resources, and creation of future programs",
+    email: "omolazabal@csu.fullerton.edu",
+    linkedin: "https://linkedin.com/in/oscar-olazabal",
+    github: "https://github.com/chabucher"
+}, {
+    id: 2,
+    image: TommyImage,
+    name: "Tommy Chao",
+    title: "Vice President",
+    description: "Primary Support on Deliverables",
+    email: "tchao770@csu.fullerton.edu",
+    github: "https://github.com/Tchao770"
+}, {
+    id: 3,
+    image: ChantalleImage,
+    name: "Chantalle Bril",
+    title: "Secretary",
+    description: "Meeting minutes, send out emails, communication",
+    email: "#"
+}, {
+    id: 4,
+    image: StevenImage,
+    name: "Steven Kha",
+    title: "Treasurer",
+    description: "Handles all financial and budgeting affairs. Maintains AS account.",
+    email: "#"
+}, {
+    id: 5,
+    image: JulianImage,
+    name: "Julian Coronado",
+    title: "Marketing Manager",
+    description: "Publicize events and programs, Social Media",
+    email: "julian.c28@gmail.com"
+}, {
+    id: 6,
+    image: DipenImage,
+    name: "Dipen Patel",
+    title: "Project Manager",
+    description: "Create new programs and manage current programs. Develop project jams and aid with workshop",
+    email: "dipenp@csu.fullerton.edu",
+    github: "https://github.com/Dipenp",
+    linkedin: "https://www.linkedin.com/in/dipen-patel89/"
+}, {
+    id: 7,
+    image: WilliamImage,
+    name: "William Clemons",
+    title: "ICC Representative",
+    description: "Attend ICC meetings once every two weeks, vote on proposals, present proposals on behalf of club",
+    email: "wclemons@csu.fullerton.edu",
+    github: "https://github.com/wclemons",
+    linkedin: "https://www.linkedin.com/in/william-clemons-0308a4105/"
+}
+];
