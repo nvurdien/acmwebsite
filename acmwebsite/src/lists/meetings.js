1a055cacb7499cb52cc1948d645f35a8798a7566
Navie Vurdien <nvurdien@gmail.com>
2018-06-09 09:22:20 -0700

    changed footer

--- acmwebsite/src/lists/meetings.js
+++ acmwebsite/src/lists/meetings.js
@@ -2,17 +2,17 @@ export default [
     {
         id: 1,
         name: 'Coding Practice',
-        room: '<room_number>',
-        start_date: '<start_date>',
-        icon: 'git-fork',
-        times: '<times>'
+        // room: '<room_number>',
+        // start_date: '<start_date>',
+        // icon: 'git-fork',
+        // times: 'TBD'
     },
     {
         id : 2,
         name: 'Project Jams',
-        room: '<room_number>',
-        start_date: '<start_date>',
-        icon: 'git-branch',
-        times: '<times>'
+        // room: '<room_number>',
+        // start_date: '<start_date>',
+        // icon: 'git-branch',
+        // times: 'TBD'
     }
 ]
