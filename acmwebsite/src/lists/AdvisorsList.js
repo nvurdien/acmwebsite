6d25f929f07a525185139dccc63542e6b5f45fe2
ozyx <jessemazzella@gmail.com>
2018-09-04 11:20:18 -0700

    Update Advisors list for Fall 2018

--- acmwebsite/src/lists/AdvisorsList.js
+++ acmwebsite/src/lists/AdvisorsList.js
@@ -1,6 +1,7 @@
 import BethImage from '../media/beth.png';
 import AnandImage from '../media/anand.jpg';
 import YunImage from '../media/yun.jpg';
+import LuisImage from '../media/luis.jpg';
 
 // id's divisible by the following:
 // divisible by 3 is blue
@@ -8,22 +9,31 @@ import YunImage from '../media/yun.jpg';
 // other is white
 
 export default [{
-    id: 1,
-    image: BethImage,
-    name: "Beth Harnick-Shapiro",
-    title: "Professor",
-    email: "Beth.Harnick.Shapiro@fullerton.edu"
-}, {
-    id: 2,
-    image: AnandImage,
-    name: "Anand Panangadan",
-    title: "Professor",
-    email: "apanangadan@Exchange.FULLERTON.EDU"
-}, {
-    id: 3,
-    image: YunImage,
-    name: "Yun Tian",
-    title: "Professor",
-    email: "ytian@fullerton.edu"
-}
+        id: 1,
+        image: BethImage,
+        name: "Beth Harnick-Shapiro",
+        title: "Professor",
+        email: "Beth.Harnick.Shapiro@fullerton.edu"
+    },
+    {
+        id: 2,
+        image: AnandImage,
+        name: "Anand Panangadan",
+        title: "Professor",
+        email: "apanangadan@Exchange.FULLERTON.EDU"
+    },
+    {
+        id: 3,
+        image: YunImage,
+        name: "Yun Tian",
+        title: "Professor",
+        email: "ytian@fullerton.edu"
+    },
+    {
+        id: 4,
+        image: LuisImage,
+        name: "Luis Larco",
+        title: "Googler in Residence",
+        email: "llarco@fullerton.edu"
+    }
 ];
